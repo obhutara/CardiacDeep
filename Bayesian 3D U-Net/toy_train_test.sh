bd93bf38e9e0c9282e93aa54efe91d50af2feb74
Omkar Bhutra <omkbh878@student.liu.se>
2021-02-27 01:31:42 +0100

    Shell script for a automated output

---
+++ Bayesian 3D U-Net/toy_train_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf toy_data toy_weights toy_images toy_predict
+python generate_toy_data.py
+python train.py with toy_config.json
+python train.py with toy_config.json "bayesian"=False
+python test.py with toy_config.json
+python test.py with toy_config.json "bayesian"=False
